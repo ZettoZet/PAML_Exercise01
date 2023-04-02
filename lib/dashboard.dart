a1838a3fe814761a3cdd0ac2e25445790de04c6f
Muhammad Farid Gilang Nuswantoro <faridganteng20050@gmail.com>
2023-04-02 11:53:18 +0700

    creating dashboard

---
+++ lib/dashboard.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, 
+        children: <Widget>[
+          Container(
+            height: 100,
+          ),
+          const Text(
+            'Pokedex.',
+            style: TextStyle(
+              color: Colors.red, 
+              fontSize: 50, 
+              fontWeight: 
+              FontWeight.bold
+            ),
+          ),
+          Container(
+            height: 50,
+          ),
+          const Text('Welcome to Pokedex'),
+          const Text('Check out all the pokemons stats here'),
+          Container(
+            height: 100,
+          ),
+          Image.asset(
+            'assets/images/pokeball.jpg',
+            width: 100,
+            height: 100,
+          ),
+          Container(
+            height: 100,
+          ),
+          
+        ],
+        )
+      ),
+    );
+  }
+}
